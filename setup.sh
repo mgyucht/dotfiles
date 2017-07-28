a41a1eaa76fd363fabaed6a9323c90652a097a2e
Miles Yucht <miles@databricks.com>
2017-07-27 19:17:19 -0700

    Some fixes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOTFILES_DIR=$(pwd)
-for dotfile in gitconfig spacemacs zshenv zshenv_local zshrc zshrc_local; do
+for dotfile in gitconfig spacemacs zprofile zshenv zshenv_local zshrc zshrc_local; do
   ln -fs $DOTFILES_DIR/$dotfile $HOME/.$dotfile
 done
 ln -fs $DOTFILES_DIR/tmux/tmux.conf $HOME/.tmux.conf
