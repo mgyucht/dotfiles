767baed40d7f76a63bf8c841c2033a60ec4e6b54
Miles Yucht <miles@databricks.com>
2017-09-14 02:05:36 -0700

    add lein

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOTFILES_DIR=$(pwd)
-for dotfile in gitconfig spacemacs zprofile zshenv zshrc; do
+for dotfile in gitconfig spacemacs zprofile zshenv zshrc lein; do
   ln -fs $DOTFILES_DIR/$dotfile $HOME/.$dotfile
 done
 ln -fs $DOTFILES_DIR/tmux/tmux.conf $HOME/.tmux.conf
